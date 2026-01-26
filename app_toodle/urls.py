b557ce795cf108bd517e66c0f236ee7875750aea
Veena S <veenasriramoju@gmail.com>
2026-01-26 22:26:58 +0000

    Update templates folder

--- app_toodle/urls.py
+++ app_toodle/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('test-static/', views.test_static, name='test_static'),
+    path('login/', views.LoginViewCustom.as_view(), name='login'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
 ]
--- app_toodle/views.py
+++ app_toodle/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 class HomePage(TemplateView):
 	"""
 	Displays home page
 	"""
 	template_name = 'index.html'
+
+class LoginViewCustom(LoginView):
+	template_name = 'account/login.html'
+
+class SignupView(CreateView):
+	form_class = UserCreationForm
+	template_name = 'account/signup.html'
+	success_url = reverse_lazy('login')
 
 def test_static(request):
 	return render(request, 'test_static.html')
